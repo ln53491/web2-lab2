1e46dd46c28f4eb96ee8492bad185e8a7419705c
Luka Nestic <luka.nestic555@gmail.com>
2023-11-01 22:49:18 +0100

    Public files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,8 @@
 import { Inter } from 'next/font/google'
-import localFont from 'next/font/local'
 import './globals.css'
 import React from "react";
 
 const inter = Inter({ subsets: ['latin'] })
-
-const modeseven = localFont({
-  src: [
-    {
-      path: '../public/fonts/modeseven.ttf',
-      weight: '400'
-    }
-  ],
-  variable: '--font-modeseven'
-})
 
 export default function RootLayout({
   children,
@@ -21,7 +10,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`${modeseven.variable} font-sans`}>
+    <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   )
